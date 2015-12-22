c8921de59f18935f1a6dc61fffa3d968feed8d5a
Jamie Swain <jpswain@gmail.com>
2015-12-21 16:13:35 -0800

    telegram question

---
+++ README.md
@@ -0,0 +1,13 @@
+# telegram-python
+
+The goal of this exercise is to satisfy the assertion provided 
+
+Telegram Problem
+
+Write a program that takes a number w, then accepts lines of text and outputs lines of text, where the output lines have as many words as possible but are never longer than w characters. Words may not be split, but you may assume that no single word is too long for a line.
+
+Please make sure the assertion succeeds and then email your answer to jamie.swain@hautelook.com.
+
+If you prefer to use another language, no problem, just make sure you get the same answer.
+
+Thanks!
---
+++ telegram-python.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+input = "HauteLook is a member-only shopping website offering flash sales and limited-time sale events with top brands in women's and men's fashion."
+
+max_length = 16
+
+expected = [
+    "HauteLook is a",
+    "member-only",
+    "shopping website",
+    "offering flash",
+    "sales and",
+    "limited-time",
+    "sale events with",
+    "top brands in",
+    "women's and",
+    "men's fashion."
+]
+
+def rewrap_lines(input, max_length):
+    # answer here:
+    pass
+
+if __name__ == "__main__":
+    print("Telegram Problem...")
+
+    answer = rewrap_lines(input, max_length)    
+    print("Answer: %s" % answer)    
+
+    assert(len(expected) > 0)
+    for i in range(0, len(expected)):
+        print('line [%i] answer: [%s], expected [%s]' % (i, answer[i], expected[i]))
+        assert(answer[i] == expected[i])
+        
+    print("SUCCESS!")
